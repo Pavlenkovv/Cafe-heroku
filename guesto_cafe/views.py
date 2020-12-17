9b00e0ac71ae13b7b69b5464ebdc4bd87775cf16
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2020-12-17 16:26:01 +0200

    -div

--- guesto_cafe/views.py
+++ guesto_cafe/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def get_main_page(request):
     context = {'title': 'Густо кафе',
                'about_title': 'Наша історія'}
     return render(request, 'index.html', context=context)
-
