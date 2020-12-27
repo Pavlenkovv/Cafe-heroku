dfc3e16bd04cc27009d9ce0aa4836520900d3c5d
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2020-12-27 12:20:25 +0200

    after lec 13. 27-12-2020

--- guesto_cafe/views.py
+++ guesto_cafe/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from menu.models import Category
 
 
 def get_main_page(request):
+    category = Category.objects.all().order_by('category_order')
+
     context = {'title': 'Густо кафе',
-               'about_title': 'Наша історія'}
+               'about_title': 'Наша історія',
+               'categories': category}
     return render(request, 'index.html', context=context)
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    category_order = models.IntegerField()
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
