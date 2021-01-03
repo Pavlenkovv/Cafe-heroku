bd004da4ab120ada62e26a44f153a23c06c4cb93
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2021-01-03 20:40:51 +0200

    booking in db

--- booking/views.py
+++ booking/views.py
@@ -6,5 +6,6 @@ def reserve_info(request):
 
     res_info = Booking.objects.get(pk=1)
     cont = {'res_info': res_info}
+    print(res_info)
 
     return render(request, 'index.html', context=cont)
