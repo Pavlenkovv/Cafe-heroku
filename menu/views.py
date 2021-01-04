2d3e5ca1d4db48911b005b82585ea2dac6c802e2
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2021-01-04 18:34:20 +0200

    done dish.html

--- menu/views.py
+++ menu/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Dish
+from booking.models import Booking
 
 def dish_detail(request, dish_id):
     dish = Dish.objects.get(pk=dish_id)
-    return HttpResponse(f'<h1>Назва: {dish.title}</h1><h1>Ціна: {dish.price}</h1><h1>Опис: {dish.description}</h1>')
+    # return HttpResponse(f'<h1>Назва: {dish.title}</h1><h1>Ціна: {dish.price}</h1><h1>Опис: {dish.description}</h1>')
+    reserve_info = Booking.objects.get(pk=1)
+    special_menu = Dish.objects.filter(category__title='Акції')
+    context = {'title': 'Густо кафе',
+                'dish': dish,
+               'reserve_info': reserve_info,
+               'special_menu': special_menu
+               }
+
+    return render(request, 'dish.html', context=context)
+
+
+
 
 def list_categories(request, dish_detail):
     context = {}
