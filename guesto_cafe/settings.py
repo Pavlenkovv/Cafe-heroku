2364434c2cc16b065c435ebed4b87c391e1f83bc
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2021-01-13 22:41:23 +0200

    13-10-2021 v.4

--- guesto_cafe/settings.py
+++ guesto_cafe/settings.py
@@ -77,8 +77,7 @@ ROOT_URLCONF = 'guesto_cafe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
