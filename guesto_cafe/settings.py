fc25c201869ae554dd8b294c13f4f03bec85c82b
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2021-01-13 20:17:23 +0200

    13-10-2021

--- guesto_cafe/settings.py
+++ guesto_cafe/settings.py
@@ -155,8 +155,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
