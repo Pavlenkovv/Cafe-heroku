cec5f3ffbf69b1aa3e72e1313e04edacdfee9b28
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2021-01-14 13:10:44 +0200

    13-10-2021 v.11

--- guesto_cafe/wsgi.py
+++ guesto_cafe/wsgi.py
@@ -14,3 +14,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guesto_cafe.settings')
 
 application = get_wsgi_application()
+
+#
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+#
+# from django.core.wsgi import get_wsgi_application
+# from dj_static import Cling
+#
+# application = Cling(get_wsgi_application())
